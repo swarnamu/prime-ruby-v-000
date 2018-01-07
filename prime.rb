4307ecf2419f4812da81968531f15e220e2a751b
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-07 21:23:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-  if 2..(number - 1).any?{|i| number % i == 0 or number == 0 or number == 1}
+  if 2..(number - 1).any?{|i| number % i == 0 or number == 0 or number == 1 or number < 0}
   #if (number % divisor == 0 || number == 0 || number == 1)
     return false
   end
