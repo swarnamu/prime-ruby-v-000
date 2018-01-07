9ad427d13cde49eaedb0649aac8bcaa93451cded
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-07 23:01:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@
 def prime?(number)
    if number < 2
      return false
-   end   
+   end
   if number == 3 || n == 2
     return true
-  end  
+  end
    if (2...number-1).any?{|i| number % i == 0}
      false
    else
