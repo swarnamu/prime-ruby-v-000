b0802f11fc2655ce655c4cd8080390b293bb3625
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-07 21:00:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-  divisor = (2..(number - 1).to_a)
+  divisor = (2..(number - 1).to_i)
   divisor.any? { |divisor| number % divisor == 0} || number == 0 || number == 1
   return false
 end
